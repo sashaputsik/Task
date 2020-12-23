a857b0b4ee81f5da63cba3f476373a77b4df1ad4
alex <sashaputsikwork@gmail.com>
2020-12-23 20:11:35 +0300

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Sasha Putsikovich on 23.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
